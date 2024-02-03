8d6365a7f302ff3144b245afb2c4f4fe7e4c6fba
Hamza <hamza.azeem2013.ha@gmail.com>
2024-02-03 21:19:18 +0200

    custom querys

--- src/main/java/com/example/springdatajdbc/controller/HomeController.java
+++ src/main/java/com/example/springdatajdbc/controller/HomeController.java
@@ -1,9 +1,14 @@
 package com.example.springdatajdbc.controller;
 
+import com.example.springdatajdbc.models.Employee;
 import com.example.springdatajdbc.repository.EmployeeRepo;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 
 @RestController
 @RequestMapping("employee")
@@ -13,7 +18,33 @@ public class HomeController {
         this.employeeRepo = employeeRepo;
     }
     @GetMapping("/count")
-    public long getCount(){
+    public long countEmp(){
         return employeeRepo.count();
     }
+    @GetMapping("/{id}")
+    public Employee findEmployee(@PathVariable("id") long id){
+        return employeeRepo.findById(id).get();
+    }
+    @GetMapping("")
+    public Iterable<Employee> findEmployees(){
+        return employeeRepo.findAll();
+    }
+    @GetMapping("/insert")
+    public Employee saveEmployee(){
+        return employeeRepo.save(new Employee(null, "WASUP", "24523"));
+    }
+    @GetMapping("/update/{id}")
+    public Employee updateEmployee(@PathVariable("id") long id){
+        return employeeRepo.save(new Employee(id, "hamzawyyy", "532459$"));
+    }
+    @GetMapping("/contains/{letters}")
+    public List<Employee> findEmpNamesLike(@PathVariable("letters") String letters){
+        letters = new String(letters+"%");
+        return employeeRepo.findByNameLike(letters);
+    }
+    @GetMapping("/startWith/{name}/{id}")
+    public List<Employee>findEmployeesLikeNameAndId(@PathVariable("name") String name, @PathVariable("id") long id){
+        return employeeRepo.findByNameStartsWithAndIdGreaterThanEqual(name, id);
+    }
+
 }
--- src/main/java/com/example/springdatajdbc/repository/EmployeeRepo.java
+++ src/main/java/com/example/springdatajdbc/repository/EmployeeRepo.java
@@ -1,9 +1,17 @@
 package com.example.springdatajdbc.repository;
 
 import com.example.springdatajdbc.models.Employee;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface EmployeeRepo extends CrudRepository<Employee, Long> {
+    @Query(value = "SELECT * FROM employee WHERE name LIKE :name")
+    List<Employee> findByNameLike(@Param("name") String name);
+    List<Employee> findByNameStartsWithAndIdGreaterThanEqual(String name, Long id);
+
 }
